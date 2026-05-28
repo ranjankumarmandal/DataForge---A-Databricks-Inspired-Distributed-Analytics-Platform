78d2fa31f749d780aa658d3898c8969bfad0581a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-28 18:24:51 +0530

    DataForge: metadata-service: intial setup

---
+++ metadata-service/src/test/java/com/ranjankumarmandal/metadata_service/MetadataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.metadata_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
