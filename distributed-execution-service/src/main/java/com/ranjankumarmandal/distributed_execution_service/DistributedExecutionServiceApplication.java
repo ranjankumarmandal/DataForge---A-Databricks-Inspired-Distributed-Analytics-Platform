e5550d400eb1b86afbf35f61520efb16e74f02dc
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-29 18:56:22 +0530

    DataForge: distribution-execution--service: intial setup

---
+++ distributed-execution-service/src/main/java/com/ranjankumarmandal/distributed_execution_service/DistributedExecutionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.distributed_execution_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedExecutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedExecutionServiceApplication.class, args);
+	}
+
+}
