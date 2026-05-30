2b134a4b4eee86e6d1520db27453d0cf2db8c09d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-30 14:14:34 +0530

    DataForge: query-egine-service: intial setup

---
+++ query-engine-service/src/main/java/com/ranjankumarmandal/query_engine_service/QueryEngineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.query_engine_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryEngineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryEngineServiceApplication.class, args);
+	}
+
+}
---
+++ query-engine-service/src/test/java/com/ranjankumarmandal/query_engine_service/QueryEngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.query_engine_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryEngineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
