e8c7a0632b31b98afa9b772ec5eb2e98f706ed20
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-30 13:03:12 +0530

    DataForge: api-gateway: completed HealthController

---
+++ api-gateway/src/main/java/com/ranjankumarmandal/api_gateway/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.ranjankumarmandal.api_gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health-check")
+    public Map<String, String> health() {
+
+        return Map.of(
+                "service",
+                "api-gateway",
+                "status",
+                "UP"
+        );
+    }
+}
