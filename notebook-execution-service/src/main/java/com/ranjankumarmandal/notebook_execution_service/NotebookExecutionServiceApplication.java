6d7ab2d1d2cb493847aed90db4b0064d9d84a5cb
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-30 11:23:48 +0530

    DataForge: notebook-execution--service:

---
+++ notebook-execution-service/src/main/java/com/ranjankumarmandal/notebook_execution_service/NotebookExecutionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.notebook_execution_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookExecutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookExecutionServiceApplication.class, args);
+	}
+
+}
---
+++ notebook-execution-service/src/test/java/com/ranjankumarmandal/notebook_execution_service/NotebookExecutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.notebook_execution_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookExecutionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
