18c66fedee2fc64ef8420935c8c547304b8511a7
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-30 14:02:01 +0530

    DataForge: auth-service: completed JwtService

---
+++ auth-service/src/main/java/com/ranjankumarmandal/auth_service/security/JwtService.java
@@ -0,0 +1,41 @@
+package com.ranjankumarmandal.auth_service.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(
+            String email,
+            String role
+    ) {
+
+        SecretKey key =
+                Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(
+                        new Date(
+                                System.currentTimeMillis()
+                                        + expiration
+                        )
+                )
+                .signWith(key)
+                .compact();
+    }
+}
